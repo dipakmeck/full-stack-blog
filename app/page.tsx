15020095f9b34556ed068ce89146e21f294ea738
dipakmeck <dipakmeck@gmail.com>
2025-07-10 15:58:14 +0530

    Provider with useSession of react

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+"use client";
+import { useSession } from "next-auth/react";
 
-export default async function Home() {
-  const data = await getServerSession(authOptions);
+export default function Home() {
+  // const data = await getServerSession(authOptions);
+  const {data, status} = useSession();
   return (
     <main>
       <div>Hello World!</div>
       <div>{JSON.stringify(data)}</div>
+      <div>{JSON.stringify(status)}</div>
     </main>
   );
 }
