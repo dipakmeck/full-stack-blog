e7516c9733a1f4e8f591ce2ea7619aab34fdec47
dipakmeck <dipakmeck@gmail.com>
2025-07-26 22:47:21 +0530

    Blogs Home With Filters and Search

---
+++ app/blogs/page.tsx
@@ -0,0 +1,29 @@
+import { categories } from "@/lib/utils"
+import { FaSearch } from "react-icons/fa"
+
+const BlogsPage = () => {
+  return (
+    <section className="w-full h-full">
+      <div className="flex flex-col gap-3 my-10 p-8">
+        <h4 className="text-3xl font-semibold">Explore Articles On Various Categories</h4>
+        <p className="text-xl font-semibold">Practical Articles For Learning Anything</p>
+      </div>
+      <nav className="bg-gray-100 border w-full flex my-4 static top-0 bg-center gap-4 h-20 md:p-8 xs:p-2 justify-between items-center">
+        <div className="mr-auto flex md:w-1/4 xs:w-2/4 items-center gap-6">
+          <p className="font-semibold text-2xl">Filter</p>
+          <select name="category" id="select" className="bg-white md:px-5 w-xs:px-2 w-3/4 mx-2 py-3 rounded-lg">
+          {categories.map((item)=><option className="rounded-md bg-gray-100" key={item.id} value={item.id}>{item.name}</option>)}
+          </select>
+        </div>
+        <div className="w-2/4 flex ml-auto md:gap-6 xs:gap-2 item-center">
+          <p className="font-semibold text-2xl">Search</p>
+          <input type="text" className="w-3/4 px-4 py-2 rounded-lg bg-white" />
+          <FaSearch className="cursor-pointer" />
+        </div>
+
+      </nav>
+    </section>
+  )
+}
+
+export default BlogsPage
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const categories = [
+  {
+      "id": "686fe4548e6426dc4251e00b",
+      "name": "Technology"
+  },
+  {
+      "id": "686fe45c8e6426dc4251e00d",
+      "name": "Education"
+  },
+  {
+      "id": "6884e9310402c5e114b5ce3f",
+      "name": "Healthcare"
+  },
+  {
+      "id": "6884e94b0402c5e114b5ce41",
+      "name": "Food"
+  },
+  {
+      "id": "6884e9560402c5e114b5ce43",
+      "name": "Politics"
+  }
+]
